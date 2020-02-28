40ef6e033ae94128d6d831ed54b8d2cc5ccf7cd7
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-02-28 22:10:37 +0530

    Assignment 4

--- 23260_AssignNo4.c
+++ 23260_AssignNo4.c
@@ -1,147 +1,219 @@
-/*
-==============================================================
-
-Roll number:- 23260
-Batch:- H 10
-
-Problem Definition:- Draw a 4X4 chessboard rotated PI/4 degrees
-with the horizontal axis. Use Bresenham algorithm to draw all 
-the lines. Use seed fill algorithm to fill black squares of 
-the rotated chessboard
-
-==============================================================
-*/
-
-#include<GL/glut.h>
-#include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-#define PI 3.141
+#include<GL/gl.h>
+#include<stdlib.h>
+#include<GL/glu.h>
+#include<GL/glut.h>
 
+float trans[3][3],ans[20][3],mat[20][3];
+int tx,ty;
+float X,Y;
 
-void bresenham(float,float,float,float);
-void plot(int ,int );
-void render();
+typedef struct pixel
+{
+	GLubyte r;
+	GLubyte g;
+	GLubyte b;
+}pixel;
+pixel b_clr,f_clr;
 
-float xx1,yy1,length;
-     
-int main(int argc, char** argv) 
+void initialize()
+{
+	float X1,Y1;
+	scanf("%f%f",&X,&Y);
+	X1=X;
+	Y1=Y;
+	for(int i=0;i<10;i+=2)
+	{
+		mat[i][0]=X;
+		mat[i][1]=Y;
+		mat[i][2]=1;
+		
+		mat[i+1][0]=X+120;
+		mat[i+1][1]=Y;
+		mat[i+1][2]=1;
+		
+		Y+=30;
+	}
+	Y=Y1;
+	for(int i=10;i<20;i+=2)
+	{
+		mat[i][0]=X;
+		mat[i][1]=Y;
+		mat[i][2]=1;
+		
+		mat[i+1][0]=X;
+		mat[i+1][1]=Y+120;
+		mat[i+1][2]=1;
+		
+		X+=30;
+	} 
+	
+	float root2 = 1/(sqrt(2));
+	trans[0][0]=trans[0][1]=trans[1][1]=root2;
+	trans[1][0]=-root2;
+	trans[0][2]=trans[1][2]=0;
+
+	tx=mat[0][0];
+	ty=mat[0][1];
+
+	trans[2][0]=((ty-tx)*root2) + tx;
+	trans[2][1]=((-ty-tx)*root2) + ty;
+	trans[2][2]=1;
+
+	for(int i=0;i<20;i++)
+	{
+		for(int j=0;j<3;j++)
+		{
+			ans[i][j]=0;
+		
+			for(int k=0;k<3;k++)
+				ans[i][j]+=mat[i][k] * trans[k][j];
+		}
+	}
+	X=X1;
+	Y=Y1;
+}
+
+void plot(int x,int y)                                 //FUNCTION TO PLOT A POINT
+{
+	glBegin(GL_POINTS);
+    	glVertex2i(x,y);
+    glEnd();  
+    glFlush();
+}
+
+void  Brezenham(int x1,int y1,int x2,int y2)
 {
 	
-     glutInit(&argc, argv);
-     glutInitDisplayMode (GLUT_SINGLE);
-     glutInitWindowSize (700, 700);
-     glutInitWindowPosition (500,0);
-     glutCreateWindow ("Assignment 4");
-     
-     printf("Enter the coordinates of left vertex:- \n");
-     printf("Enter the x coordinte:- ");
-     scanf("%f",&xx1);
-     printf("Enter the y coordinate:- ");
-     scanf("%f",&yy1);
-     printf("Enter the length of the square:- ");
-     scanf("%f",&length);
-   
-     glutDisplayFunc(render);
-     glutMainLoop();
-     return 0;
-} 
-
-     
-void  bresenham(float x1,float y1,float x2,float y2)
-{
-     int i;
-     float dx,dy,xinc,yinc,inc1,inc2,p,x,y;                  
-     dx=x2-x1;
-     dy=y2-y1;
-    
-     if(dx<0)
-          dx=-dx;
-     if(dy<0)
-          dy=-dy;
-    
-     xinc=1;
-     yinc=1;
-    
-     if(x2<x1)                                    
-          xinc=-1;
-     if(y2<y1)
-          yinc=-1;
-    
-     x=x1;
-     y=y1;
-
-     if(dx>dy)
-     {
-    		plot((x-y)*cos(PI/4),(x+y)*cos(PI/4));
-          
-          p=2*dy-dx;          
-          inc1=2*(dy-dx);
-          inc2=2*dy;
-          for(i=0;i<dx;i++)
-          {
-	          if(p>0)
-         		{
-		          y+=yinc;
-         			p+=inc1;
-         		}
-	          else
-		          p+=inc2;
-         		x+=xinc;
-	    		plot((x-y)*cos(PI/4),(x+y)*cos(PI/4));
-         	
-          }
-     }
-     else	                                          
-     {
-  		plot((x-y)*cos(PI/4),(x+y)*cos(PI/4));
-      	
-      	p=2*dx-dy;
-      	inc1=2*(dx-dy);
-      	inc2=2*dx;
-      	for(i=0;i<dy;i++)
-      	{
-      		if(p>0)
-      		{
-			     x+=xinc;
-			     p+=inc1;
-		     }
-		     else
-			     p+=inc2;
-		     y+=yinc;
-         		plot((x-y)*cos(PI/4),(x+y)*cos(PI/4));
-	     }
-     }
+    int incx,incy,inc1,inc2,p,x,y;                    //INITIALIZATION
+	int dy=y2-y1;
+    int dx=x2-x1;
+    if(dx<0)
+    	dx=-dx;
+    if(dy<0)
+    	dy=-dy;
+    incx=1;
+    incy=1;
+    if(x2<x1)                                        //IF FINAL CO-ORDINATE IS LESS THAN INITIAL THE INCREMENT ID NEGATIVE   
+    	incx=-1;
+    if(y2<y1)
+    	incy=-1;
+    x=x1;
+    y=y1;
+    if(dx>dy)                                         //TOTAL NUMBER OF STEPS IS DX IF IT IS GREATER THAN DY
+    {
+    	plot(x,y);
+    	p=2*dy-dx;
+    	inc1=2*(dy-dx);
+    	inc2=2*dy;
+    	for(int i=0;i<dx;i++)
+    	{
+    		if(p>0)
+    		{
+    			y+=incy;
+    			p+=inc1;
+    		}
+    		else
+    			p+=inc2;
+    		x+=incx;
+    		plot(x,y);
+    	}
+    }
+    else	                                          //TOTAL NUMBER OF STEPS IS DY IF IT IS GREATER THAN DX
+    {
+	 	plot(x,y);
+	 	p=2*dx-dy;
+	 	inc1=2*(dx-dy);
+	 	inc2=2*dx;
+	 	for(int i=0;i<dy;i++)
+	 	{
+	 		if(p>0)
+	 		{
+				x+=incx;
+				p+=inc1;
+			}
+			else
+			{
+				p+=inc2;
+			}
+			y+=incy;
+			plot(x,y);
+		}
+	}
 }
 
-     
-void plot(int x,int y)
+void boundary_fill(float x,float y)
 {
-     glBegin(GL_POINTS);
-     glVertex2i(x,y);
-     glEnd();
-     glFlush();
+	pixel c;
+	glReadPixels(x,y,1,1,GL_RGB,GL_UNSIGNED_BYTE,&c);
+	
+	if((c.r!=b_clr.r || c.g!=b_clr.g ||  c.b!=b_clr.b) && (c.r!=f_clr.r || c.g!=f_clr.g || c.b!=f_clr.b))
+	{
+		glBegin(GL_POINTS);
+     		glVertex2d(x,y);
+     		glColor3ub(f_clr.r,f_clr.g,f_clr.b);
+     		glEnd();
+     		glFlush();
+     		boundary_fill(x,y+1);
+     		boundary_fill(x-1,y);
+     		boundary_fill(x,y-1);
+     		boundary_fill(x+1,y);
+    }
 }
-	     
-void render()
+
+void init(void)
 {
-	int temp;
-     glClearColor(0.0, 0.0, 0.0, 0.0);
-     glPointSize(3.0);
-     glClear(GL_COLOR_BUFFER_BIT);
-     glMatrixMode(GL_PROJECTION);
-     glLoadIdentity();
-     gluOrtho2D(-780,780,-420,420);
-     
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+    glColor3i(0,0,0);
+    glClear(GL_COLOR_BUFFER_BIT);
+   	gluOrtho2D(0,750,0,750);
+}
 
-     //coordinates
-     glPointSize(2.0);
-     glColor3f(1.0,1.0,1.0);
-     
-     for(temp=0;temp<=length;temp+=length/4)
-     {
-          bresenham(xx1+temp,yy1,xx1+temp,yy1+length);
-          bresenham(xx1,yy1+temp,xx1+length,yy1+temp);
-     }
-
+void draw()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	int i;
+	for(i=0;i<20;i+=2)
+		Brezenham(ans[i][0],ans[i][1],ans[i+1][0],ans[i+1][1]);
+		
+	float factor=60/(sqrt(2));
+	float add=30/sqrt(2);
+	Y=Y+add;
+	
+	boundary_fill(X,Y);
+	
+	for(int i=2;i<=6;i++)
+		boundary_fill(X,Y+i*add);
+		
+	Y=Y+factor;
+	boundary_fill(X+factor,Y);
+	boundary_fill(X+factor,Y+factor);
+	boundary_fill(X-factor,Y);
+	boundary_fill(X-factor,Y+factor);
+	
+	glEnd();
+	glFlush();
+}
+	
+int main(int argc,char **argv)
+{
+	initialize();
+	
+	b_clr.r=0;
+	b_clr.g=0;
+	b_clr.b=0;
+	
+	f_clr.r=150;
+	f_clr.g=150;
+	f_clr.b=150;
+	
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitWindowPosition(0,0);
+	glutInitWindowSize(750,750);
+    glutCreateWindow("Chessboard");
+	init();
+	glutDisplayFunc(draw);
+	glutMainLoop();
 }
